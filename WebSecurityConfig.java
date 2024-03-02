1fe55522fdbe37ad874e6e2eb68908fe40597b6a
JuliodelJunco <117302342+JuliodelJunco@users.noreply.github.com>
2024-03-02 19:42:27 +0100

    routing
    finished unless some error when merged

--- WebSecurityConfig.java
+++ WebSecurityConfig.java
@@ -1,9 +1,6 @@
-package security;
-
-import java.security.SecureRandom;
+package webapp4.main.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,13 +8,14 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.security.SecureRandom;
+
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     RepositoryUserDetailsService userDetailsService;
 
-    @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(10, new SecureRandom());
     }
@@ -29,20 +27,45 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    protected void configure(HttpSecurity https) throws Exception {
 
         // Public pages
-        http.authorizeRequests().antMatchers("/").permitAll();
-        http.authorizeRequests().antMatchers("/login").permitAll();
-        http.authorizeRequests().antMatchers("/loginerror").permitAll();
-        http.authorizeRequests().antMatchers("/logout").permitAll();
+        https.authorizeRequests().antMatchers("/").permitAll();
+        https.authorizeRequests().antMatchers("/login").permitAll();
+        https.authorizeRequests().antMatchers("/login_error").permitAll();
+        https.authorizeRequests().antMatchers("/logout").permitAll();
+        https.authorizeRequests().antMatchers("/chart").permitAll();
+        https.authorizeRequests().antMatchers("/password").permitAll();
+        https.authorizeRequests().antMatchers("/register").permitAll();
+        https.authorizeRequests().antMatchers("/waiting").permitAll();
+        https.authorizeRequests().antMatchers("/error_404").permitAll();
+        https.authorizeRequests().antMatchers("/error_403").permitAll();
+
+
+
+
 
         // Private pages
+        https.authorizeRequests().antMatchers("/profile").hasAnyRole("USER");
+        https.authorizeRequests().antMatchers("/transfers").hasAnyRole("USER");
+        https.authorizeRequests().antMatchers("/transfer").hasAnyRole("USER");
+        https.authorizeRequests().antMatchers("/data").hasAnyRole("USER");
+        https.authorizeRequests().antMatchers("/loan_request").hasAnyRole("USER");
+        https.authorizeRequests().antMatchers("/validation").hasAnyRole("ADMIN");
+        https.authorizeRequests().antMatchers("/charge").hasAnyRole("ADMIN");
+        https.authorizeRequests().antMatchers("/admin_profile").hasAnyRole("ADMIN");
+
 
         // Login form
-
+        https.formLogin().loginPage("/login");
+        https.formLogin().usernameParameter("username");
+        https.formLogin().passwordParameter("password");
+        https.formLogin().defaultSuccessUrl("/profile");
+        https.formLogin().failureUrl("/login_error");
 
         // Logout
+        https.logout().logoutUrl("/logout");
+        https.logout().logoutSuccessUrl("/login");
 
     }
 }
