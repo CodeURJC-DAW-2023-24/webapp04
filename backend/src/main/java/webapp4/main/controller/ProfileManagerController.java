75b3d9e9739b678770e838ed2bc4c29b1bb76cdd
JuliodelJunco <117302342+JuliodelJunco@users.noreply.github.com>
2024-03-04 02:41:30 +0100

    Create ProfileManagerController.java

---
+++ backend/src/main/java/webapp4/main/controller/ProfileManagerController.java
@@ -0,0 +1,12 @@
+package webapp4.main.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProfileManagerController {
+    @GetMapping("/profile_manager")
+    public String password(Model model){
+        return "validate_account_manager";
+    }
+}
