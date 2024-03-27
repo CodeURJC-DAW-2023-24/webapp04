de7400a143cd77310b529e2482c2b6c6ca44bb27
Cristian A. Vlad <cav.contact.pr@gmail.com>
2024-03-27 18:02:29 +0100

    Updated docker folder

--- docker/create_image.sh
+++ docker/create_image.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 docker build -t Cristian1A/urjc_bank -f ../docker/Dockerfile ../
-docker-compose -f ../docker/docker-compose.yml up
+docker push Cristian1A/urjc_bank
+docker-compose up
