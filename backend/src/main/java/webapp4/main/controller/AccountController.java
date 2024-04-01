07719c90c6e023532214884d35f5801ae3da7d52
JuliodelJunco <117302342+JuliodelJunco@users.noreply.github.com>
2024-04-01 23:19:34 +0200

    Update AccountController.java

--- backend/src/main/java/webapp4/main/controller/AccountController.java
+++ backend/src/main/java/webapp4/main/controller/AccountController.java
@@ -8,31 +8,45 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import webapp4.main.model.Account;
+import webapp4.main.model.AccountDTO;
 import webapp4.main.repository.AccountRepository;
+import webapp4.main.service.AccountService;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class AccountController {
     @Autowired
     private AccountRepository accountRepository;
     private List<Account> allAccounts;
+    @Autowired
+    private AccountService accountService;
+
     @ModelAttribute
-    public void auxMethod(Model model){
+    public void auxMethod(Model model) {
         allAccounts = accountRepository.findAll();
     }
+
     @GetMapping("/account")
-    public String account(Model model){
+    public String account(Model model) {
         return "account_page";
     }
+
     @GetMapping("/account_load")
     @ResponseBody
-    public List<Account> accountManager(@RequestParam int startIndex, @RequestParam int chunkSize){
-        if (startIndex + chunkSize < allAccounts.size()){
-            return allAccounts.subList(startIndex, startIndex + chunkSize);
-        }
-        return new ArrayList<>();
+    public List<AccountDTO> accountManager(@RequestParam int startIndex, @RequestParam int chunkSize) {
+        int endIndex = Math.min(startIndex + chunkSize, allAccounts.size());
+        return allAccounts.subList(startIndex, endIndex).stream().map(account -> {
+            AccountDTO dto = new AccountDTO();
+            dto.setNip(account.getNIP());
+            dto.setIban(account.getIBAN());
+            dto.setName(account.getName());
+            dto.setSurname(account.getSurname());
+            dto.setImageBase64(accountService.getProfilePicBase64(account.getNIP()));
+            return dto;
+        }).collect(Collectors.toList());
     }
 }
 
