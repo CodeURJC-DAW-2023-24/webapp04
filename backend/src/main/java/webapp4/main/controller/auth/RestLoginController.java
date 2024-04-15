48d274d8e9280d24f3eb7b3f7b145a34f3eab34e
Cristian A. Vlad <cav.contact.pr@gmail.com>
2024-04-15 11:46:41 +0200

    jwt package update 2

--- backend/src/main/java/webapp4/main/controller/auth/RestLoginController.java
+++ backend/src/main/java/webapp4/main/controller/auth/RestLoginController.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import webapp4.main.security.jwt.AuthResponse;
@@ -15,12 +16,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @RestController
-@RequestMapping("/api/auth")
-public class RestLoginController{
+@RequestMapping("/api")
+public class RestLoginController {
     @Autowired
     private UserLoginService userService;
 
-    @Operation(summary = "Login")
+    @Operation(summary = "User login through form")
     @ApiResponses(value = {
             @ApiResponse(
                     responseCode = "200",
@@ -43,15 +44,46 @@ public class RestLoginController{
                     content = @Content(mediaType = "application/json")
             )
     })
-    @PostMapping("/login")
-    public ResponseEntity<AuthResponse> login(
+    @PostMapping(value = "/login", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public ResponseEntity<AuthResponse> loginForm(
             @CookieValue(name = "accessToken", required = false) String accessToken,
             @CookieValue(name = "refreshToken", required = false) String refreshToken,
-            @RequestBody LoginRequest loginRequest){
+            @ModelAttribute LoginRequest loginRequest) {
         return userService.login(loginRequest, accessToken, refreshToken);
     }
 
-    @Operation(summary = "Refresh")
+    @Operation(summary = "User login through form")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "User logged in successfully",
+                    content = @Content(mediaType = "application/json")
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Bad request",
+                    content = @Content(mediaType = "application/json")
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "Unauthorized",
+                    content = @Content(mediaType = "application/json")
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Forbidden",
+                    content = @Content(mediaType = "application/json")
+            )
+    })
+    @PostMapping(value = "/login", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<AuthResponse> loginJson(
+            @CookieValue(name = "accessToken", required = false) String accessToken,
+            @CookieValue(name = "refreshToken", required = false) String refreshToken,
+            @RequestBody LoginRequest loginRequest) {
+        return userService.login(loginRequest, accessToken, refreshToken);
+    }
+
+    @Operation(summary = "Checks if the user logged in is the same as the user in the token")
     @ApiResponses(value = {
             @ApiResponse(
                     responseCode = "200",
@@ -74,20 +106,22 @@ public class RestLoginController{
                     content = @Content(mediaType = "application/json")
             )
     })
+
     @PostMapping("/refresh")
     public ResponseEntity<AuthResponse> refreshToken(
-            @CookieValue(name = "refreshToken", required = false) String refreshToken){
+            @CookieValue(name = "refreshToken", required = false) String refreshToken) {
         return userService.refresh(refreshToken);
     }
 
-    @Operation(summary = "Logout")
+    @Operation(summary = "Logs out the user")
     @ApiResponse(
             responseCode = "200",
             description = "User is logged out",
             content = @Content(mediaType = "application/json")
     )
     @PostMapping("/logout")
-    public ResponseEntity<AuthResponse> logOut(HttpServletRequest request, HttpServletResponse response){
+    public ResponseEntity<AuthResponse> logOut(HttpServletRequest request, HttpServletResponse response) {
         return ResponseEntity.ok(new AuthResponse(AuthResponse.Status.SUCCESS, userService.logout(request, response)));
     }
+
 }
