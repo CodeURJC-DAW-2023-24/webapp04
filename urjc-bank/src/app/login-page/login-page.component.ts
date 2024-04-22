0376a86571bdda2da16baf187352c696d6cc92bb
Cristian A. Vlad <cav.contact.pr@gmail.com>
2024-04-22 16:04:30 +0200

    Added register page component

--- urjc-bank/src/app/login-page/login-page.component.ts
+++ urjc-bank/src/app/login-page/login-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeadComponent } from '../head/head.component';
+import { ShortFooterComponent } from '../short-footer/short-footer.component';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [HeadComponent, ShortFooterComponent],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
---
+++ urjc-bank/src/app/short-footer/short-footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-short-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './short-footer.component.html',
+  styleUrl: './short-footer.component.css'
+})
+export class ShortFooterComponent {
+
+}
