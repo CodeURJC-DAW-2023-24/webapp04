78d8339314c181cdf43d6b6b69125dd2daf6ae44
DavidCC <96200005+DavidCCGithubURJC@users.noreply.github.com>
2024-06-25 00:30:07 +0200

    account service updated

--- frontend/src/app/services/account.service.ts
+++ frontend/src/app/services/account.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+
+
 export interface Account {
   nip: string;
   iban: string;
@@ -13,8 +15,16 @@ export interface Account {
 @Injectable({
   providedIn: 'root'
 })
+
 export class AccountService {
+
+  private register_url = "/api/accounts";
+
   constructor(private http: HttpClient) { }
+  
+  register(inputUser: string, name:string, surname:string, password: string, confirmPassword:string): Observable<any>{
+    return this.http.post<any>(this.register_url, {inputUser, name, surname, password, confirmPassword});
+  }
 
   loadMoreAccounts(page: number, size: number): Observable<Account[]> {
     const params = new HttpParams()
