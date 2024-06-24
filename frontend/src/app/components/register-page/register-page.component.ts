a23900996bd2ff54f32ffa2486414b2eb886491a
DavidCC <96200005+DavidCCGithubURJC@users.noreply.github.com>
2024-06-25 00:26:37 +0200

    Implemented register page

--- frontend/src/app/components/register-page/register-page.component.ts
+++ frontend/src/app/components/register-page/register-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AccountService } from '../../services/account.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-page',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrl: './register-page.component.css'
 })
 export class RegisterPageComponent {
+  constructor(private accountService: AccountService, private router: Router) {}
+
+  onSubmit(inputUser: string, name: string, surname: string, password: string, confirmPassword: string): void {
+    this.accountService.register(inputUser, name, surname, password, confirmPassword).subscribe(
+      data => {
+        this.router.navigate(['/login']);
+      },
+    );
+  }
 
 }
